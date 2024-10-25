5808874aaed2f0e88b4af321b6b57072a58829df
Rodion Yanushkevich <rodion.yanushkevich@mail.ru>
2024-10-25 22:46:00 +0700

    img src

--- js/generate-comments-template.js
+++ js/generate-comments-template.js
@@ -6,36 +6,34 @@ const commentContainer = document.createDocumentFragment();
 
 function generateComments (pictureSrc) {
   // упрости перепиши
-  const url = new URL(pictureSrc);
-  const imagePath = url.pathname;
-  const formattedPath = imagePath.slice(1);
+  const formattedPath = pictureSrc.substring(pictureSrc.indexOf('photos/'));
   const pictureData = photoDataArray.find((imgUrl) => imgUrl.url === formattedPath);
 
-  if (pictureData && pictureData.comments) {
-    pictureData.comments.forEach ((comment) => {
-      const newCommentTemplate = document.createElement('LI');
-      newCommentTemplate.classList.add('social__comment');
+  //   if (pictureData && pictureData.comments) {
+  pictureData.comments.forEach ((comment) => {
+    const newCommentTemplate = document.createElement('LI');
+    newCommentTemplate.classList.add('social__comment');
 
-      const newCommentImage = document.createElement('IMG');
-      newCommentImage.classList.add('social__picture');
-      newCommentImage.src = comment.avatar;
-      newCommentImage.alt = comment.name;
-      newCommentImage.setAttribute('width', '35');
-      newCommentImage.setAttribute('height', '35');
-      newCommentTemplate.appendChild(newCommentImage);
+    const newCommentImage = document.createElement('IMG');
+    newCommentImage.classList.add('social__picture');
+    newCommentImage.src = comment.avatar;
+    newCommentImage.alt = comment.name;
+    newCommentImage.setAttribute('width', '35');
+    newCommentImage.setAttribute('height', '35');
+    newCommentTemplate.appendChild(newCommentImage);
 
-      const newCommentDescription = document.createElement('P');
-      newCommentDescription.classList.add('social__text');
-      newCommentDescription.textContent = comment.message;
-      newCommentTemplate.appendChild(newCommentDescription);
+    const newCommentDescription = document.createElement('P');
+    newCommentDescription.classList.add('social__text');
+    newCommentDescription.textContent = comment.message;
+    newCommentTemplate.appendChild(newCommentDescription);
 
-      commentContainer.appendChild(newCommentTemplate);
-    });
+    commentContainer.appendChild(newCommentTemplate);
+  });
 
-    newCommentArea.innerHTML = '';
-    newCommentArea.appendChild(commentContainer);
-  }
-
+  newCommentArea.innerHTML = '';
+  newCommentArea.appendChild(commentContainer);
 }
+
+// }
 
 export { generateComments};
