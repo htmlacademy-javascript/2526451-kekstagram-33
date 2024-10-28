17f32108d802212b708701c0cf1c140e3a86028f
Rodion Yanushkevich <rodion.yanushkevich@mail.ru>
2024-10-28 19:26:47 +0700

    проверка ао регулярным выражениям. дубликатамб длинны

--- js/validator.js
+++ js/validator.js
@@ -13,22 +13,56 @@ const hashtagsPristneValidator = new Pristine(uploadForm);
 // захочешь потом можно добавить все описания
 
 
-function validateHashtagsInput(value) {
+function validateAllhashtags(hashtagsArray) {
 
-  const hashTags = value.split(' ');
-  const allHashgagsTrue = hashTags.every((hashTag) => hashtagsRegular.test(hashTag));
-
-  function doubledHashtags (){
-
-
+  if (!hashtagsArray.every((hashTag) => hashtagsRegular.test(hashTag))) {
+    /* eslint-disable */
+  //  console.log('Хештег должен начинаться с № и не иметь пробелов и без спецсимволов');
+   /* eslint-enable */
+    return false;
   }
-
-  return allHashgagsTrue;
+  return true;
 }
 
 
-hashtagsPristneValidator.addValidator(hashtagsInput,validateHashtagsInput);
+function hasDuplicateHashtags(hashtagsArray) {
+  const noDuplicate = {};
+  for (let i = 0; i < hashtagsArray.length; i++) {
+    const hashtag = hashtagsArray[i];
+    if (!noDuplicate[hashtag]) {
+      noDuplicate[hashtag] = true;
+    } else {
+      /* eslint-disable */
+      console.log('дубль');
+         /* eslint-enable */
+      return false;
+    }
+  }
+  return true;
+}
 
+function maxHashtagsValidation (hashtagsArray,maxHashtags) {
+  if (hashtagsArray. length > maxHashtags) {
+    console.log('не больше 5ти хештегов');
+    return false;
+  }
+  return true;
+}
+
+function validateHashtagsInput(value) {
+  const hastTagsArray = value.split(' ');
+  const maxHashtags = 5;
+
+
+  const allHashtagsRegularValid = validateAllhashtags(hastTagsArray);
+  const noneDuplicates = hasDuplicateHashtags(hastTagsArray);
+  const maxHashtagsValid = maxHashtagsValidation(hastTagsArray,maxHashtags);
+
+
+  return allHashtagsRegularValid && noneDuplicates && maxHashtagsValid;
+}
+
+hashtagsPristneValidator.addValidator(hashtagsInput,validateHashtagsInput);
 
 uploadForm.addEventListener('submit', (evt) => {
   evt.preventDefault();
