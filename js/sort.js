fc03286285a7129a43b03f17d3cbee102b4d243a
Rodion Yanushkevich <rodion.yanushkevich@mail.ru>
2024-11-18 15:25:09 +0700

    оптимизирована функция сортировки с дебаунсом

--- js/sort.js
+++ js/sort.js
@@ -10,39 +10,6 @@ const RANDOM_PICTURES_COUNT = 10;
 
 const mostPopularArray = photoDataArray.slice();
 
-
-function comparePopular (a,b) {
-  return b.comments.length - a.comments.length;
-}
-
-
-function showDeafaultPictures(evt) {
-  popularBtn.classList.remove('img-filters__button--active');
-  randomBtn.classList.remove('img-filters__button--active');
-
-  evt.target.classList.add('img-filters__button--active');
-
-  const picturesImg = pictures.querySelectorAll('.picture');
-  picturesImg.forEach((picture) => picture.remove());
-
-  evt.target.classList.add('img-filters__button--active');
-
-  generatePictures(photoDataArray);
-}
-
-function showPopularPictures(evt) {
-
-  deffaultBtn.classList.remove('img-filters__button--active');
-  randomBtn.classList.remove('img-filters__button--active');
-
-  evt.target.classList.add('img-filters__button--active');
-
-  const picturesImg = pictures.querySelectorAll('.picture');
-  picturesImg.forEach((picture) => picture.remove());
-
-  generatePictures(mostPopularArray.sort(comparePopular));
-}
-
 //алгоритм Фишера-Йетса..
 function shuffle(array) {
   const shuffledArray = array.slice();
@@ -53,20 +20,60 @@ function shuffle(array) {
   return shuffledArray.slice(0,RANDOM_PICTURES_COUNT);
 }
 
+
+function comparePopular (a,b) {
+  return b.comments.length - a.comments.length;
+}
+
+function debounce (callback, timeoutDelay = 500) {
+  let timeoutId;
+  return (...rest) => {
+    clearTimeout(timeoutId);
+
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+}
+
+const deboucedGeneratePictures = debounce((array) => {
+  const picturesImg = pictures.querySelectorAll('.picture');
+  picturesImg.forEach((picture) => picture.remove());
+  generatePictures(array);
+});
+
+function showDeafaultPictures(evt) {
+  popularBtn.classList.remove('img-filters__button--active');
+  randomBtn.classList.remove('img-filters__button--active');
+
+  evt.target.classList.add('img-filters__button--active');
+  deboucedGeneratePictures(photoDataArray);
+}
+
+function showPopularPictures(evt) {
+  deffaultBtn.classList.remove('img-filters__button--active');
+  randomBtn.classList.remove('img-filters__button--active');
+
+  evt.target.classList.add('img-filters__button--active');
+
+  deboucedGeneratePictures(mostPopularArray.sort(comparePopular));
+}
+
+
 function showTenRandomPictures(evt) {
   deffaultBtn.classList.remove('img-filters__button--active');
   popularBtn.classList.remove('img-filters__button--active');
 
   evt.target.classList.add('img-filters__button--active');
 
-  const picturesImg = pictures.querySelectorAll('.picture');
-  picturesImg.forEach((picture) => picture.remove());
+  // const picturesImg = pictures.querySelectorAll('.picture');
+  // picturesImg.forEach((picture) => picture.remove());
 
-  generatePictures(shuffle(photoDataArray));
+  deboucedGeneratePictures(shuffle(photoDataArray));
 }
+
 
 popularBtn.addEventListener('click', showPopularPictures);
 deffaultBtn.addEventListener('click', showDeafaultPictures);
 randomBtn.addEventListener('click', showTenRandomPictures);
+
 
 export {sortMenu};
